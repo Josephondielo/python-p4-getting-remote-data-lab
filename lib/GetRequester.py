07fe9726d516631c62cb17666329d74e209024d7
Joseph <josebonagain@gmail.com>
2025-11-20 09:41:23 +0300

    passesd

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,16 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        """Send GET request and return response as a string."""
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response.text  # string, not bytes
 
     def load_json(self):
-        pass
+        """Return the JSON response as Python object (list/dict)."""
+        response_body = self.get_response_body()
+        return json.loads(response_body)
